ce6feb090b1c9d4dc8942abb6df2236f9f52a1a5
solomon wamuutu <solomonwamutu16@gmail.com>
2024-03-16 14:21:32 +0300

    api

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const apikey = "594249d28be6426b817a234aa0001e72";
+const blogContainer = document.getElementById("blog-container");
+
+function SearchRandomNews(){
+    try{
+
+    }
+    catch(error){
+        console.error("Error searching Random News",error);
+        return[];
+    }
+}
