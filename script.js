532b7f88569e78f84862f10c50c51d1d65f7aabd
solomon wamuutu <solomonwamutu16@gmail.com>
2024-03-16 14:48:52 +0300

    api

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const apikey = "594249d28be6426b817a234aa0001e72";
 const blogContainer = document.getElementById("blog-container");
 
-function SearchRandomNews(){
+async function SearchRandomNews(){
     try{
         const apiUrl = `https://newsapi.org/v2/everything?q=tesla&pageSize=10&apikey =${apikey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data)
+
     }
     catch(error){
         console.error("Error searching Random News",error);
